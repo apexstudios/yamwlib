6e75164075fbdf783db4bb3e83561360c7be85e4
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-11 02:21:28 +0100

    Adding a stubbed Templater class. Missing proper markup management (it does not know how to get the manager), and unit tests

---
+++ src/YamwLibs/Infrastructure/Templater/Templater.php
@@ -0,0 +1,108 @@
+<?php
+namespace YamwLibs\Infrastructure\Templater;
+
+/**
+ * Processes and generates templates
+ *
+ * @author AnhNhan <anhnhan@outlook.com>
+ * @package YamwLibs
+ * @subpackage Templater
+ */
+class Templater
+{
+    private static $template_name;
+    private static $template_code;
+    private static $proc_crit;
+    private static $proc_uncrit;
+
+    public static function loadTemplate($template)
+    {
+        if (!file_exists($template) || !is_readable($template)) {
+            return false;
+        }
+
+        self::$template_name = $template;
+        self::$template_code = file_get_contents($template);
+
+        return true;
+    }
+
+    public static function noTemplate()
+    {
+        return static::loadTemplate(Config::get('template.default.nt'));
+    }
+
+    public static function isUsingTemplate()
+    {
+        return static::getCurrentTemplate() == Config::get('template.default.nt');
+    }
+
+    public static function getCurrentTemplate()
+    {
+        return static::$template_name;
+    }
+
+    public static function generateTemplate()
+    {
+        if (
+            empty(self::$template_code) &&
+            !self::loadTemplate(Config::get('template.default'))
+        ) {
+            throw new \RuntimeException('No template could be loaded!');
+        }
+
+        self::processTemplate(false);
+        self::processTemplate(true);
+    }
+
+    public static function loadCache($cache)
+    {
+        self::$template_name = 'cached';
+        self::$template_code = $cache;
+        self::$proc_uncrit = true;
+    }
+
+    public static function generateCache()
+    {
+        if (
+            empty(self::$template_code) ||
+            !self::loadTemplate(Config::get('template.default'))
+        ) {
+            throw new RuntimeException('No template could be loaded!');
+        }
+
+        self::processTemplate(false);
+    }
+
+    public static function processTemplate($crit = true)
+    {
+        // Stub - we'll replace this with a proper one another time
+        $markupMgr = new MarkupManager;
+
+        if (($crit ? self::$proc_crit : self::$proc_uncrit)) {
+            return false;
+        }
+
+        foreach ($markupMgr as $markup) {
+            $pattern = '{'.$markup->getPattern().'}';
+            if (strpos(self::$template_code, $pattern) !== false) {
+                self::$template_code = str_replace(
+                    $pattern,
+                    $markup->getContent(),
+                    self::$template_code
+                );
+            }
+        }
+
+        if ($crit) {
+            self::$proc_crit = true;
+        } else {
+            self::$proc_uncrit = true;
+        }
+    }
+
+    public static function retrieveTemplate()
+    {
+        return self::$template_code;
+    }
+}
