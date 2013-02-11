6756f54546850ab02e4036413de07db2d927973d
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-11 07:03:13 +0100

    added a basic unit test for Templater

---
+++ src/YamwLibs/Infrastructure/Templater/tests/TemplaterTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace YamwLibs\Infrastructure\Templater;
+
+/**
+ * @author AnhNhan <anhnhan@outlook.com>
+ */
+class TemplaterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSimpleTemplateGeneration()
+    {
+        $markupMgr = MarkupManager::getInstance();
+        $filePath = __DIR__ . "/mocks/basic_template";
+        $cmpPath = __DIR__ . "/mocks/generated_template";
+        $date = date(DATE_RFC2822, time());
+
+        $markupName = new Markup\SimpleTemplateMarkup(
+            'name',
+            'NAME',
+            'John Doe'
+        );
+
+        $markupTime = new Markup\MethodInvocationMarkup(
+            'time',
+            'TIME',
+            array(
+                'date',
+                array(DATE_RFC2822, time())
+            )
+        );
+
+        $markupMgr->registerMarkup($markupName)->registerMarkup($markupTime);
+
+        Templater::loadCache(file_get_contents($filePath));
+        Templater::generateTemplate();
+
+        $generatedTemplate = Templater::retrieveTemplate();
+        self::assertEquals(
+            trim(file_get_contents($cmpPath)) . " " . $date,
+            trim($generatedTemplate)
+        );
+    }
+}
