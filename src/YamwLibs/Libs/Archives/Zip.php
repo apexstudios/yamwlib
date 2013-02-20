7972dda2c3cfc0166a71d4083b3b4d39912bf8ce
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-20 18:53:20 +0100

    adding new zip function

--- src/YamwLibs/Libs/Archives/Zip.php
+++ src/YamwLibs/Libs/Archives/Zip.php
@@ -58,4 +58,42 @@ class Zip
             return false;
         }
     }
+
+    /**
+     * Taken from php.net
+     *
+     * @param type $folder
+     * @param type $zipFile
+     * @param type $subfolder
+     * @return boolean
+     */
+    public static function folderToZip($folder, &$zipFile, $subfolder = null)
+    {
+        if ($zipFile == null) {
+            // no resource given, exit
+            return false;
+        }
+        // we check if $folder has a slash at its end, if not, we append one
+        $folder .= end(str_split($folder)) == "/" ? "" : "/";
+        $subfolder .= end(str_split($subfolder)) == "/" ? "" : "/";
+        // we start by going through all files in $folder
+        $handle = opendir($folder);
+        while ($f = readdir($handle)) {
+            if ($f != "." && $f != "..") {
+                if (is_file($folder . $f)) {
+                    // if we find a file, store it
+                    // if we have a subfolder, store it there
+                    if ($subfolder != null)
+                        $zipFile->addFile($folder . $f, $subfolder . $f);
+                    else
+                        $zipFile->addFile($folder . $f);
+                } elseif (is_dir($folder . $f)) {
+                    // if we find a folder, create a folder in the zip
+                    $zipFile->addEmptyDir($f);
+                    // and call the function again
+                    self::folderToZip($folder . $f, $zipFile, $f);
+                }
+            }
+        }
+    }
 }
