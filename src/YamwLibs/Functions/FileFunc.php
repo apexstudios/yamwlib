74dd2386938db80969a1d4c5545d04909f3a3a58
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-20 15:15:21 +0100

    fixing FileFunc class

--- src/YamwLibs/Functions/FileFunc.php
+++ src/YamwLibs/Functions/FileFunc.php
@@ -14,7 +14,7 @@ class FileFunc
     {
         $files = array_diff(scandir($dir), array('.', '..'));
         foreach ($files as $file) {
-            (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
+            (is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
         }
         return rmdir($dir);
     }
