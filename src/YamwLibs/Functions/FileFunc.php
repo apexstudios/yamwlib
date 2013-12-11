7a8c93d7289149b07169a8f71ae93677ec780ced
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-11 17:35:03 +0100

    Adding `FileFunc::recursiveScanForDirectories`

--- src/YamwLibs/Functions/FileFunc.php
+++ src/YamwLibs/Functions/FileFunc.php
@@ -23,4 +23,38 @@ class FileFunc
         }
         return rmdir($dir);
     }
+    
+    /**
+     * 
+     * @param string $dirName The directory to start from
+     * @param string $extension
+     * @return array All files matching with the specified extension (or any)
+     * in a directory and its subdirectories
+     */
+    function recursiveScanForDirectories($dirName, $extension = '.*')
+    {
+        $dirList = scandir($dirName);
+        $files = array();
+
+        foreach ($dirList as $entry) {
+            if (in_array($entry, array('.', '..'))) {
+                continue;
+            }
+
+            $name = $dirName . '/' . $entry;
+            if (is_dir($name)) {
+                $files = array_merge($files, self::recursiveScanForDirectories($name));
+            } else {
+                if (
+                    !strpos($entry, '.') !== 0 &&
+                    preg_match('/'.$extension.'$/', $entry)
+                ) {
+                    $files[] = $name;
+                }
+            }
+        }
+
+        return $files;
+    }
+
 }
