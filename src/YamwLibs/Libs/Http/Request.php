ce618635284c6fdf7fddda81e86ba917926d85c1
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-11 17:04:10 +0100

    Adding `Request` object from Yamw

---
+++ src/YamwLibs/Libs/Http/Request.php
@@ -0,0 +1,201 @@
+<?php
+namespace YamwLibs\Libs\Http;
+
+use \YamwLibs\Infrastructure\Config\Config;
+
+/**
+ * A request object contains all information passed over by an external request,
+ * be it Http or an internal request like when including a module within another
+ * module.
+ *
+ * @author AnhNhan <anhnhan@outlook.com>
+ * @package Yamw
+ * @subpackage Lib
+ */
+class Request
+{
+    private $params = array();
+
+    /**
+     * Populates the Request with the $params passed to it and, if applicable,
+     * loads the default values for request information that has not been passed
+     * over
+     *
+     * @since 3.0
+     * @param array $params
+     */
+    public function init(array $params = array())
+    {
+        if (!$params) {
+            $params = array(
+                'module' => Config::get('default.module'),
+                'action' => Config::get('default.action'),
+                'section' => Config::get('default.section')
+            );
+        }
+
+        if (!isset($params['section']) || !$params['section']) {
+            $params['section'] = Config::get('default.section');
+        }
+
+        if (!isset($params['module']) || !$params['module']) {
+            $params['module'] = Config::get('default.module');
+        }
+
+        if (!isset($params['action']) || !$params['action']) {
+            $params['action'] = Config::get('default.action');
+        }
+
+        if (isset($_COOKIE['mybb_sid'])) {
+            $params['cookie-sid'] = $_COOKIE['mybb_sid'];
+        } else {
+            $params['cookie-sid'] = false;
+        }
+
+        $this->populate($params);
+    }
+
+    /**
+     * Assigns the passed data to the local key-value store, overwriting any
+     * existing entries.
+     *
+     * @param array $data
+     * @since 5.0
+     */
+    public function populate(array $data)
+    {
+        foreach ($data as $key => $value) {
+            $this->setValue($key, $value);
+        }
+    }
+
+    /**
+     * Populates the stack with an array of parameters, which keys will get
+     * receive a prefix
+     *
+     * @param array $global
+     * @param string $prefix
+     */
+    private function populatePrefix(array $global, $prefix = 'get')
+    {
+        $data = array();
+
+        foreach ($global as $key => $value) {
+            $data[$prefix . '-' . strtolower($key)] = $value;
+        }
+
+        $this->populate($data);
+    }
+
+    private function populateSuperGlobal(
+        array $global,
+        $prefix = 'get',
+        array $populate_objects = null
+    ) {
+        if (!$populate_objects) {
+            $this->populatePrefix($global, $prefix);
+        } else {
+            $data = array();
+
+            foreach ($populate_objects as $key => $value) {
+                if (is_numeric($key)) {
+                    $data[$value] = isset($global[$value]) ?
+                        $global[$value] : null;
+                } else {
+                    $data[$key] = isset($global[$key]) ? $global[$key] : $value;
+                }
+            }
+
+            $this->populatePrefix($data, $prefix);
+        }
+    }
+
+    /**
+     * Populates the Request object with data from the post superglobal
+     *
+     * @since 5.0
+     */
+    public function populateFromPost(array $populate_objects = null)
+    {
+        $this->populateSuperGlobal($_POST, 'post', $populate_objects);
+    }
+
+    /**
+     * Populates the Request object with data from the get superglobal
+     *
+     * @since 5.0
+     */
+    public function populateFromGet(array $populate_objects = null)
+    {
+        $this->populateSuperGlobal($_GET, 'get', $populate_objects);
+    }
+
+    /**
+     * Populates the Request object with data from the get superglobal
+     *
+     * @since 5.0
+     */
+    public function populateFromServer(array $populate_objects = null)
+    {
+        $this->populateSuperGlobal($_SERVER, 'server', $populate_objects);
+    }
+
+    public function populateFromCookies(array $populate_objects = null)
+    {
+        $this->populateSuperGlobal($_COOKIE, 'cookie', $populate_objects);
+    }
+
+    /**
+     * Returns the value associated with the $name key
+     *
+     * @since 5.0
+     * @param string $name
+     * @throws \InvalidArgumentException
+     *
+     * @return multiptype: string|bool|number
+     */
+    public function getValue($name, $default_value = null)
+    {
+        if (isset($this->params[$name])) {
+            return $this->params[$name];
+        } else {
+            // Invalid key
+            return $default_value;
+        }
+    }
+
+    public function getValueInt($name, $default_value = null)
+    {
+        return (int)$this->getValue($name, $default_value);
+    }
+
+    /**
+     * Sets the key $name to the given $value
+     *
+     * @param string $name
+     * @param mixed $value
+     * A scalar value
+     */
+    public function setValue($name, $value)
+    {
+        if (!is_scalar($value) && !is_null($value)) {
+            throw new \InvalidArgumentException(
+                '$value must be a "normal" value!'
+            );
+        }
+
+        $this->params[$name] = $value;
+    }
+
+    /**
+     * Returns whether a value has been associated with the given $name key
+     *
+     * @param string $name
+     *
+     * @return boolean
+     */
+    public function valueExists($name)
+    {
+        return isset($this->params[$name]);
+    }
+}
---
+++ src/YamwLibs/Libs/Http/tests/RequestTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace YamwLibs\Libs\Http;
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    public $className = '\Yamw\Lib\Request';
+
+    public function testInitializationPopulatesInitialState()
+    {
+        $request = new Request;
+        // Just to be sure that it is non-existent
+        self::assertSame(null, $request->getValue('heyho'));
+        self::assertSame(null, $request->getValue('dzde'));
+
+        $data = array(
+            'dzde' => 'default',
+            'heyho' => 'it exists'
+        );
+        $request->init($data);
+
+        // Now the data exists
+        self::assertSame('it exists', $request->getValue('heyho'));
+        self::assertSame('default', $request->getValue('dzde'));
+
+        // Init with other data
+        $data = array(
+            'dzde' => 'wut?',
+            'heyho' => 'yup, it\'s true'
+        );
+        $request->init($data);
+
+        // Now some other data exists
+        self::assertSame('yup, it\'s true', $request->getValue('heyho'));
+        self::assertSame('wut?', $request->getValue('dzde'));
+    }
+
+    public static function testCanSetRequestValues()
+    {
+        $request = new Request;
+        // Init with other data
+        $data = array(
+            'section' => 'wut?',
+        );
+        $request->init($data);
+
+        self::assertSame('wut?', $request->getValue('section'));
+
+        // Set it to a new value
+        $request->setValue('section', 'whatever');
+        self::assertSame('whatever', $request->getValue('section'));
+    }
+
+    public function testCanPopulateFromGet()
+    {
+        $request = new Request;
+        // Just filling the superglobal...
+        $_GET['hi'] = 'hey';
+        self::assertArrayHasKey('hi', $_GET);
+
+        $request->populateFromGet();
+
+        self::assertEquals('hey', $request->getValue('get-hi'));
+    }
+
+    public function testCanPopulateFromPost()
+    {
+        $request = new Request;
+        // Just filling the superglobal...
+        $_POST['hi'] = 'hey';
+        self::assertArrayHasKey('hi', $_POST);
+
+        $request->populateFromPost();
+
+        self::assertEquals('hey', $request->getValue('post-hi'));
+    }
+}
