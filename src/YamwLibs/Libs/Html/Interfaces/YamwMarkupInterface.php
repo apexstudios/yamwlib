8152631e966daff26ef5ae8f4b9db89d18a45a51
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-19 13:14:35 +0100

    Removing some not-always-useful methods from markup interface

--- src/YamwLibs/Libs/Html/Interfaces/YamwMarkupInterface.php
+++ src/YamwLibs/Libs/Html/Interfaces/YamwMarkupInterface.php
@@ -3,10 +3,6 @@ namespace YamwLibs\Libs\Html\Interfaces;
 
 interface YamwMarkupInterface
 {
-    public function isPretty();
-    public function makePretty();
-    public function makeDirty();
-    
     /**
      * Generates and returns the string representation of this markup
      *
